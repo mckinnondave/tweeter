54f0b177ef01d23317c17a00ee60c3e6795d9716
Dave McKinnon <dave.w.mckinnon@gmail.com>
2022-03-30 02:20:39 +0000

    remove example data code

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,17 +1,5 @@
 $(document).ready(function() {
-
-  // const tweetData = {
-  //   "user": {
-  //     "name": "Newton",
-  //     "avatars": "https://i.imgur.com/73hZDYK.png",
-  //       "handle": "@SirIsaac"
-  //     },
-  //   "content": {
-  //       "text": "If I have seen further it is by standing on the shoulders of giants"
-  //     },
-  //   "created_at": 1461116232227
-  // }
-
+  
   const data = [
     {
       "user": {
