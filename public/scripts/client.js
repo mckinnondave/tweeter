479a8da50f5c21d822b8bec481a452ef7d0631a8
Dave McKinnon <dave.w.mckinnon@gmail.com>
2022-03-30 01:34:20 +0000

    add renderTweets to sort through array of objects and post respective tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,17 +5,42 @@
  */
 $(document).ready(function() {
 
-  const tweetData = {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
+  // const tweetData = {
+  //   "user": {
+  //     "name": "Newton",
+  //     "avatars": "https://i.imgur.com/73hZDYK.png",
+  //       "handle": "@SirIsaac"
+  //     },
+  //   "content": {
+  //       "text": "If I have seen further it is by standing on the shoulders of giants"
+  //     },
+  //   "created_at": 1461116232227
+  // }
+
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
         "handle": "@SirIsaac"
       },
-    "content": {
+      "content": {
         "text": "If I have seen further it is by standing on the shoulders of giants"
       },
-    "created_at": 1461116232227
-  }
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ]
 
   const createTweetElement = (tweetData) => {
     const { user, content, created_at } = tweetData;
@@ -43,9 +68,13 @@ $(document).ready(function() {
     `);
     return $tweet
   }
-  
-  const $tweet = createTweetElement(tweetData);
-  console.log($tweet);
-  $(".mainContainer").append($tweet);
+
+  const renderTweets = (tweetObjects) => {
+    for (const object of tweetObjects) {
+      const $tweet = createTweetElement(object);
+      $(".new-tweet").append($tweet);
+    }
+  }
+  renderTweets(data);
 
 })
